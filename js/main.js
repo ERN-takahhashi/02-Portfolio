d903e1dca5e7758816b2dc0b64adb0c37e132fa0
{e.takahashi} <{er.takahashi4@gmail.com}>
2026-01-23 07:24:30 +0900

    1/22-2分 push

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const modal = document.getElementById('ingredientModal');
+const openButtons = document.querySelectorAll('.c-button--ingredient');
+const closeBtn = document.querySelector('.c-modal__close');
+const overlay = document.querySelector('.c-modal__overlay');
+
+openButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    modal.classList.add('is-active');
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('is-active');
+});
+
+overlay.addEventListener('click', () => {
+  modal.classList.remove('is-active');
+});
